7e9c5a63a74830bc0249aaa28da59e84d7f318c2
Jhon Jairo Valdés <jhon.valdes@softgic.co>
2023-10-23 11:39:40 -0500

    segundo commit

--- src/App.js
+++ src/App.js
@@ -8,11 +8,30 @@ function App() {
   const [result, setResult] = useState("");
 
   const calculateIMC = () => {
+    //Validación de datos
+    if (isNaN(weight) || isNaN(height) || weight === "" || height === "") {
+      setResult({
+        error: "Por Favor ingrese datos válidos para peso y altura.",
+      });
+      return;
+    }
     //Convierto altura a metros
     const heightInMeters = height / 100;
     //Calculo IMC
     const imc = weight / (heightInMeters * heightInMeters).toFixed(2);
-    setResult(imc);
+    //Clasificación según peso
+    let classification = "";
+    if (imc < 18.5) {
+      classification = "Bajo Peso";
+    } else if (imc >= 18.5 && imc <= 24.9) {
+      classification = "Normal";
+    } else if (imc >= 25 && imc <= 29.9) {
+      classification = "Sobrepeso";
+    } else {
+      classification = "Obesidad";
+    }
+
+    setResult({ imc, classification });
   };
   return (
     <div className="App">
@@ -34,7 +53,13 @@ function App() {
         />
       </div>
       <button onClick={calculateIMC}>Calcular IMC</button>
-      {result && <p>Tu IMC es {result}</p>}
+      {result && result.error && <p style={{ color: "red" }}>{result.error}</p>}
+      {result && result.imc && (
+        <div>
+          <p>Tu IMC es: {result.imc}</p>
+          <p>Clasificación: {result.classification}</p>
+        </div>
+      )}
     </div>
   );
 }
